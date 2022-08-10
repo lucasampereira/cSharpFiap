9c4614fe36031bb1e31d1026b1eb2ec2a1214912
eliasromero72 <102391069+eliasromero72@users.noreply.github.com>
2022-08-10 08:35:31 -0300

    newcSharp

---
+++ 00-ParImparPositivoNegativo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+/*
+int numeroDeVezes = 5;
+for(int i = 0; i < numeroDeVezes; i++) {
+    Console.WriteLine("Bem-vindo ao curso de .Net! " + i);
+}
+*/
+
+int numero = 2;
+
+if(numero < 0) {
+    Console.WriteLine("O número " + numero + " é negativo");
+} else if(numero == 0) {
+    Console.WriteLine("O número " + numero + " é nulo");
+} else {
+    Console.WriteLine("O número " + numero + " é positivo");
+}
+
+if(numero == 0) {
+    Console.WriteLine("Zero não é par nem ímpar");
+} else if(numero % 2 == 0) {
+    Console.WriteLine("O número " + numero + " é par");
+} else {
+    Console.WriteLine("O número " + numero + " é ímpar");
+}
